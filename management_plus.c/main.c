e7732002d8c95abd36adbc009162dcfcc3f2e784
lee <lee>
2021-05-29 20:21:35 +0900

    Initial Commit

---
+++ management_plus.c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  management_plus.c
+//
+//  Created by Ju-hyung Lee on 2021/05/29.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
